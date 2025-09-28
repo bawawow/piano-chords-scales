f62d351ee6f61606c6b39d489f6375c5cc3f5170
bawawow <czarrazc27@gmail.com>
2025-09-28 09:18:42 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showChord(chord) {
+  document.getElementById("output").innerText = "Chord: " + chord;
+}
+
+function showScale(scale) {
+  document.getElementById("output").innerText = "Scale: " + scale;
+}
